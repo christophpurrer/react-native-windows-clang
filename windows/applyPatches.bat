a07124fa52d47583ad592c7e267311894385f207
Christoph Purrer <christophpurrer@gmail.com>
2020-05-18 03:04:13 -0700

    React-native-windows 0.62 updates
    - Set correct entries in win32.exe.manifest
    - Add patch file for back listener problem in ReactRootControl.cpp
    - Update yarn.lock

--- windows/applyPatches.bat
+++ windows/applyPatches.bat
@@ -3,7 +3,7 @@ set CURRENT_PATH=%cd%
 cd %~dp0
 
 :: Patch ReactRootView2.cpp
-"C:\Program Files\Git\usr\bin\patch.exe" -i ReactRootView2.cpp.patch ..\node_modules\react-native-windows\Microsoft.ReactNative\ReactRootView2.cpp
+"C:\Program Files\Git\usr\bin\patch.exe" -i ReactRootControl.cpp.patch ..\node_modules\react-native-windows\Microsoft.ReactNative\Views\ReactRootControl.cpp
 
 :: Creating missing pch.h
 echo #pragma once > ..\node_modules\react-native-windows\Microsoft.ReactNative.Cxx\pch.h
